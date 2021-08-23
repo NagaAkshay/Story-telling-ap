e903eb70498277eb05dbdb806803347a0aa2e3aa
NagaAkshay <69960504+NagaAkshay@users.noreply.github.com>
2021-08-23 15:16:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC2SSG6cNV3KT_qXNlUMcVvb8UoK66C-uo",
+    authDomain: "story-telling-app-c8bf4.firebaseapp.com",
+    projectId: "story-telling-app-c8bf4",
+    storageBucket: "story-telling-app-c8bf4.appspot.com",
+    messagingSenderId: "96004920694",
+    appId: "1:96004920694:web:ae093d03f1fc81ca73d657"
+  }
+
+ 
